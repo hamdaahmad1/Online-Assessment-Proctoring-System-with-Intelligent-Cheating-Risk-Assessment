ee74ef920fb00e5143346ff59e2023e5b694a4a3
hamda ahmad <hamdaahmad572@gmail.com>
2026-08-15 14:41:21 +0500

    Setup the PYTHON virtual enviroment

---
+++ AI_Service/app/services/vision_service.py
@@ -0,0 +1,13 @@
+# Object and Person Detection Service using YOLOv11
+
+def detect_objects(image_path: str) -> list:
+    """
+    Detects prohibited objects like phones, books, calculators.
+    """
+    pass
+
+def count_persons(image_path: str) -> int:
+    """
+    Counts the number of persons in the frame.
+    """
+    pass
